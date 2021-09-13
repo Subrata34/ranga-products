485f8c4aa8ce633b52f3c38a276561b6f23766fe
Subrata34 <subratamodak3491@gmail.com>
2021-09-13 14:56:16 +0600

    files

--- js/app.js
+++ js/app.js
@@ -39,7 +39,8 @@ const addToCart = (id, price) => {
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = Math.floor(element);
+  console.log(converted);
   return converted;
 };
 
@@ -48,12 +49,13 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = Math.abs(total).toFixed(3);
+  
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = Math.abs(value).toFixed(3);
 };
 
 // update delivery charge and total Tax
@@ -71,11 +73,13 @@ const updateTaxAndCharge = () => {
     setInnerText("delivery-charge", 60);
     setInnerText("total-tax", priceConverted * 0.4);
   }
+  updateTotal();
 };
 
 //grandTotal update function
 const updateTotal = () => {
   const grandTotal = getInputValue("price") + getInputValue("delivery-charge") +getInputValue("total-tax");
     console.log(grandTotal);
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = grandTotal.toFixed(3);
 };
+loadProducts();
